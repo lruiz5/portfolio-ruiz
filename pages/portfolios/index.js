f1d3b67be6d3694c10dfe7fc1cf9cfcef90afa90
Luis Ruiz <l.ruiz5@live.com>
2022-11-22 22:27:50 -0800

    fetching from our backend api and styling changes

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,32 +1,42 @@
 import Link from "next/link";
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
-import { useGetPosts } from "@/actions";
 import { useGetUser } from "@/actions/user";
+import PortfolioApi from "@/lib/api/portfolios";
+import { Row, Col } from "reactstrap";
+import PortfolioCard from "@/components/PortfolioCard";
 
-const Portfolios = () => {
-  const { data, error, loading: loadingData } = useGetPosts();
+const Portfolios = ({ portfolios }) => {
   const { data: user, loading } = useGetUser();
 
-  const renderPosts = (portfolios) => {
-    return portfolios.map((portfolio) => (
-      <li key={portfolio.id}>
-        <Link href={`portfolios/${portfolio.id}`}>{portfolio.title}</Link>
-      </li>
-    ));
-  };
   return (
     <>
       <BaseLayout user={user} loading={loading}>
-        <BasePage>
-          <h1>Portfolios Page</h1>
-          {loadingData && <p>Loading Data...</p>}
-          {data && <ul>{renderPosts(data)}</ul>}
-          {error && <div className="alert alert-danger">{error.message}</div>}
+        <BasePage header="Portfolios" className="portfolio-page">
+          <Row>
+            {portfolios.map((portfolio) => (
+              <Col key={portfolio._id} md="4">
+                <PortfolioCard portfolio={portfolio} />
+              </Col>
+            ))}
+          </Row>
         </BasePage>
       </BaseLayout>
     </>
   );
 };
+
+//function called during buildtime
+//creates a static page with dynamic data
+export async function getStaticProps() {
+  const json = await new PortfolioApi().getAll();
+
+  const portfolios = json.data;
+  return {
+    props: {
+      portfolios,
+    },
+  };
+}
 
 export default Portfolios;
