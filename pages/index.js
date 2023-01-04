8302c0a2a74b90c5c031b95af52528c2957d9ac2
Luis Ruiz <l.ruiz5@live.com>
2023-01-03 16:37:06 -0800

    added flip animation to card on front, color changes on flip interval

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import BaseLayout from "@/components/layouts/BaseLayout";
 import Typed from "typed.js";
 import { useGetUser } from "@/actions/user";
 import Image from "next/image";
 
-const roles = ["Developer", "Tech Lover", "Team Player", "React JS"];
+const roles = ["Developer", "Tech Lover", "Team Player", "React JS", "Next JS"];
 const Index = () => {
   const el = useRef(null);
+  const flipInterval = useRef();
+  const [isFlipping, setIsFlipping] = useState(false);
   const tagline = `Let's take a look at my work.`;
   const { data: user, loading } = useGetUser();
+
+  useEffect(() => {
+    startAnimation();
+    return () => flipInterval.current && clearInterval(flipInterval.current);
+  }, []);
+
+  const startAnimation = () => {
+    flipInterval.current = setInterval(() => {
+      setIsFlipping((previousFlip) => !previousFlip);
+    }, 10000);
+  };
 
   useEffect(() => {
     const typed = new Typed(el.current, {
@@ -35,7 +48,7 @@ const Index = () => {
       user={user}
       loading={loading}
       navClass={"transparent"}
-      className="cover"
+      className={`cover ${isFlipping ? "cover-orange" : "cover-blue"}`}
     >
       <div className="main-section">
         <div className="background-image">
@@ -50,8 +63,8 @@ const Index = () => {
           <Row>
             <Col md="6">
               <div className="hero-section">
-                <div className={`flipper`}>
-                  <div className="back">
+                <div className={`flipper ${isFlipping ? "isFlipping" : ""}`}>
+                  <div className="front">
                     <div className="hero-section-content">
                       <h2> Full Stack Web Developer </h2>
                       <div className="hero-section-content-intro">
@@ -67,6 +80,24 @@ const Index = () => {
                     />
                     <div className="shadow-custom">
                       <div className="shadow-inner"> </div>
+                    </div>
+                  </div>
+                  <div className="back">
+                    <div className="hero-section-content">
+                      <h2> Full Stack Web Developer </h2>
+                      <div className="hero-section-content-intro">
+                        Have a look at my portfolio and job history.
+                      </div>
+                    </div>
+                    <Image
+                      width={450}
+                      height={550}
+                      alt="featured"
+                      className="image"
+                      src="/images/section-2.png"
+                    />
+                    <div className="shadow-custom-orange">
+                      <div className="shadow-inner-orange"> </div>
                     </div>
                   </div>
                 </div>
